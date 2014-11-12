82c974c3b6afa1c16c4ea4e80e8bba717086b8bd
kyohiro <kyohiro@gmail.com>
2014-11-13 00:17:40 +0800

    Add a O(log(m+n)) solution

--- 002-medium of two sorted arrays/Solution002.java
+++ 002-medium of two sorted arrays/Solution002.java
@@ -1,5 +1,8 @@
+import java.util.Arrays;
+
 public class Solution002 {
-    public double findMedianSortedArrays(int A[], int B[]) {
+    //O(m) solution
+    public double findMedianSortedArrays1(int A[], int B[]) {
         int sizeA = A.length, sizeB = B.length;
         int sizeC = A.length + B.length;
         int[] C = new int[sizeC];
@@ -8,23 +11,52 @@ public class Solution002 {
         for (;idxC < sizeC && idxA < sizeA && idxB < sizeB;++idxC) {
             if (A[idxA] <= B[idxB])
                 C[idxC] = A[idxA++];
-            else 
+            else
                 C[idxC] = B[idxB++];
         }
 
         if (idxA != sizeA) {
             while (idxA < sizeA)
                 C[idxC++] = A[idxA++];
-        }        
+        }
         if (idxB != sizeB) {
             while (idxB < sizeB)
                 C[idxC++] = B[idxB++];
         }
 
 
-        if (sizeC % 2 == 0) 
-            return (double)(C[sizeC/2-1] + C[sizeC/2]) / 2; 
+        if (sizeC % 2 == 0)
+            return (double)(C[sizeC/2-1] + C[sizeC/2]) / 2;
         else
             return C[sizeC / 2];
     }
+
+    //O(log(m+n)) solution
+    public double findMedianSortedArrays(int A[], int B[]) {
+        int total = A.length + B.length;
+        if (total % 2 != 0)
+            return findKth(A, B, total/2+1);
+        else
+            return (double)(findKth(A, B, total/2) + findKth(A, B, total/2+1))/2;
+    }
+
+    private double findKth(int[] A, int[] B, int k) {
+        if (A.length > B.length)
+            return findKth(B, A, k);
+        if (A.length == 0)
+            return B[k - 1];
+        if (k == 1)
+            return A[0] < B[0] ? A[0] : B[0];
+
+        int pa = k/2 < A.length ? k/2 : A.length;
+        int pb = k - pa;
+        if (A[pa-1] < B[pb-1])
+            return findKth(Arrays.copyOfRange(A, pa, A.length), B, k-pa);
+        else if (A[pa-1] > B[pb-1])
+            return findKth(A, Arrays.copyOfRange(B, pb, B.length), k-pb);
+        else
+            return A[pa-1];
+
+    }
+
 }
