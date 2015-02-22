887f7b6eab6cb86d3126f042d9ea4a2ee69ec239
kyohiro <kyohiro@gmail.com>
2015-02-23 00:22:28 +0800

    Enrich problem #1 cpp version

---
+++ 001-two sum/Solution001.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	//Attempt1: 40ms 
+	//This solution is problematic cause using map will override the first element's index if there're duplicates 
+	//But it appears to be correct as it says there'd be only solution
+	vector<int> twoSum(vector<int> &numbers, int target) {
+		map<int, int> numbersMap;
+		for (auto idx = 0; idx != numbers.size(); ++idx) 
+			numbersMap.insert(pair<int, int>(numbers[idx], idx + 1));
+
+		for (auto idx = 0; idx != numbers.size(); ++idx) {
+			int remaining = target - numbers[idx];
+			auto findTarget = numbersMap.find(remaining);
+			if (findTarget != numbersMap.end()) {
+				int secondIdx = findTarget->second;
+				if (idx + 1 != secondIdx) {
+					vector<int> result{ idx + 1, secondIdx };
+					sort(result.begin(), result.end());
+					return result;
+				}
+			}
+		}
+	}
+
+	//Attempt2: 33ms
+	vector<int> twoSum2(vector<int> &numbers, int target) {
+		int size = numbers.size(), l = 0, r = size - 1;
+
+		vector<pair<int, int>> numberPairs;
+		for (auto idx = 0; idx != size; ++idx)
+			numberPairs.push_back(make_pair(numbers[idx], idx + 1));
+		sort(numberPairs.begin(), numberPairs.end());
+
+		while (l < r) {
+			int sum = numberPairs[l].first + numberPairs[r].first;
+			if (sum < target) ++l;
+			else if (sum > target) --r;
+			else {
+				vector<int> result{ numberPairs[l].second, numberPairs[r].second };
+				sort(result.begin(), result.end());
+				return result;
+			}
+		}
+	}
+	
+};
+
+int main()
+{
+	vector<int> numbers{ -1, -2, -3, -4, -5 };
+	Solution s;
+	vector<int> ret = s.twoSum(numbers, -8);
+
+	return 0;
+}
--- 001-two sum/Solution001.java
+++ 001-two sum/Solution001.java
@@ -1,4 +1,8 @@
-public class Solution002 {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution001 {
+    //Attemp1: 249ms
     public int[] twoSum(int[] numbers, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < numbers.length; ++i)
@@ -8,7 +12,7 @@ public class Solution002 {
             if (map.containsKey(target - numbers[i])) {
                 int another = map.get(target - numbers[i]);
                 if (another != i)
-                    return new int[]{i+1, another+1};
+                    return another > i ? new int[]{i + 1, another + 1} : new int[]{another + 1, i + 1};
             }
 
         throw new IllegalArgumentException("No valid solition for input.");
