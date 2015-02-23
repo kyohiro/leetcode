5fccd9e32af9e2d2bfa93975eedb523e43d6a81c
kyohiro <kyohiro@gmail.com>
2015-02-23 10:43:43 +0800

    Update Solution001.java

--- 001-two sum/Solution001.java
+++ 001-two sum/Solution001.java
@@ -2,7 +2,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution001 {
-    //Attemp1: 249ms
+    //Attempt1: 249ms
+    //Attempt2: 225ms
     public int[] twoSum(int[] numbers, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < numbers.length; ++i)
@@ -12,7 +13,7 @@ public class Solution001 {
             if (map.containsKey(target - numbers[i])) {
                 int another = map.get(target - numbers[i]);
                 if (another != i)
-                    return another > i ? new int[]{i + 1, another + 1} : new int[]{another + 1, i + 1};
+                    return new int[]{i + 1, another + 1}；
             }
 
         throw new IllegalArgumentException("No valid solition for input.");
