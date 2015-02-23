92427b243ff63a0fde25e5737667cc3458a54c78
kyohiro <kyohiro@gmail.com>
2015-02-23 21:50:58 +0800

    update indentation for solution002.cpp

--- 002-add two numbers/Solution002.cpp
+++ 002-add two numbers/Solution002.cpp
@@ -1,43 +1,43 @@
 #include <stdio.h>
 
 struct  ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
 };
 
 //Attempt1: 58ms
 class Solution {
 public:
-    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-        ListNode *head = new ListNode(0), *current = head;
-        int step = 0;
+  ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+    ListNode *head = new ListNode(0), *current = head;
+    int step = 0;
 
-        while (l1 != NULL || l2 != NULL || step != 0) {
-            int value = step;
-            if (l1 != NULL) {
-                value += l1->val;
-                l1 = l1->next;
-            }
-            if (l2 != NULL) {
-                value += l2->val;
-                l2 = l2->next;
-            }
+    while (l1 != NULL || l2 != NULL || step != 0) {
+      int value = step;
+      if (l1 != NULL) {
+        value += l1->val;
+        l1 = l1->next;
+      }
+      if (l2 != NULL) {
+        value += l2->val;
+        l2 = l2->next;
+      }
 
-            current->val = value % 10;
-            step = value / 10;
-            if (l1 != NULL || l2 != NULL || step != 0) {
-                ListNode *next = new ListNode(0);
-                current->next = next;
-                current = next;
-            }
-        }
-
-        return head;
+      current->val = value % 10;
+      step = value / 10;
+      if (l1 != NULL || l2 != NULL || step != 0) {
+        ListNode *next = new ListNode(0);
+        current->next = next;
+        current = next;
+      }
     }
+
+    return head;
+  }
 };
 
 int main() {
-    return 0;
+  return 0;
 }
 
