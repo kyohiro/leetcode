0aa67be4f16fba0a8c0aeef855f567b9c1f83d69
kyohiro <kyohiro@gmail.com>
2015-02-27 23:32:46 +0800

    update solution006.cpp

--- 006-zigzag conversion/Solution006.cpp
+++ 006-zigzag conversion/Solution006.cpp
@@ -3,22 +3,22 @@
 //Attempt1: 26ms
 class Solution006 {
 public:
-    string convert(string s, int nRows) {
-      if (s.length() == 0 || nRows < 0)
-        return "";
-      if (nRows == 1)
-        return s;
+  string convert(string s, int nRows) {
+    if (s.length() == 0 || nRows < 0)
+    return "";
+    if (nRows == 1)
+    return s;
 
-      string ret;
-      int offset = 2 * nRows - 2;
-      for (int j = 0; j < nRows; ++j) {
-        for (int i = j; i < s.length(); i += offset) {
-          ret.append(1, s[i]);
-          if (j != 0 && j != nRows - 1 && i + 2 * nRows - 2 - 2 * j < s.length())
-            ret.append(1, s[i + 2 * nRows - 2 - 2 * j]);
-        }
-      }
-      return ret;
+    string ret;
+    int offset = 2 * nRows - 2;
+    for (int j = 0; j < nRows; ++j) {
+    for (int i = j; i < s.length(); i += offset) {
+      ret.append(1, s[i]);
+      if (j != 0 && j != nRows - 1 && i + 2 * nRows - 2 - 2 * j < s.length())
+      ret.append(1, s[i + 2 * nRows - 2 - 2 * j]);
     }
+    }
+    return ret;
+  }
 
 };
