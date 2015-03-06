886ccc9b6e1ff4012fadd20a56314f60d5566af0
kyohiro <kyohiro@gmail.com>
2015-03-07 00:12:58 +0800

    add solution 012

---
+++ 012-roman to integer/Solution012.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <map>
+
+class Solution012 {
+//Attempt1: 85ms
+public:
+  int romanToInt(string s) {
+    std::map<char, int> symbolMap = {{'M', 1000}, {'D', 500}, {'C', 100}, {'L', 50}, {'X', 10}, {'V', 5}, {'I', 1}};
+
+    int result = 0;
+    for (auto i = 0; i != s.length(); ++i) {
+      if (i != s.length() - 1 && symbolMap[s[i]] < symbolMap[s[i + 1]]) {
+        result += symbolMap[s[i + 1]];
+        result -= symbolMap[s[i]];
+        ++i;
+      }
+      else {
+        result += symbolMap[s[i]];
+      }
+    }
+
+    return result;
+  }
+};
