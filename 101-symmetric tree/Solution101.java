c899ea97a2afd189e6b5abaf3f9a8d33637d9cbd
kyohiro <kyohiro@gmail.com>
2015-03-29 23:05:31 +0800

    update solution 101

--- 101-symmetric tree/Solution101.java
+++ 101-symmetric tree/Solution101.java
@@ -8,6 +8,7 @@
  * }
  */
 public class Solution {
+    //Attempt1: 243ms - recursive
     public boolean isSymmetric(TreeNode root) {
         if (root == null)
             return true;
@@ -23,6 +24,35 @@ public class Solution {
         else
             return isSymmetricForTwo(left.left, right.right) && isSymmetricForTwo(left.right, right.left);
 
+    }
+
+    //Attempt2: 311ms - non-recursive
+    public boolean isSymmetric2(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null))
+            return true;
+
+        Stack<TreeNode> leftStack = new Stack<TreeNode>();
+        Stack<TreeNode> rightStack = new Stack<TreeNode>();
+        leftStack.push(root.left);
+        rightStack.push(root.right);
+
+        while (!leftStack.isEmpty() && !rightStack.isEmpty()) {
+            TreeNode left = leftStack.pop();
+            TreeNode right = rightStack.pop();
+            if (left == null && right == null)
+                continue;
+            else if (left == null || right == null)
+                return false;
+            else if (left.val != right.val) 
+                return false;
+
+            leftStack.push(left.left);
+            rightStack.push(right.right);
+            leftStack.push(left.right);
+            rightStack.push(right.left);
+        }
+
+        return true;
     }
 
 }
