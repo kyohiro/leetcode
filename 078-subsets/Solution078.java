2d7d7e0b6d495fd88a485f2a26b127283ea29b42
kyohiro <kyohiro@gmail.com>
2015-05-24 23:15:10 +0800

    update solution 078

--- 078-subsets/Solution078.java
+++ 078-subsets/Solution078.java
@@ -3,22 +3,19 @@ public class Solution054 {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> results = new ArrayList<List<Integer>>();    
         List<Integer> list = new ArrayList<Integer>();
-        results.add(list);
         Arrays.sort(nums);
         subSubsets(nums, 0, list, results);
 
         return results;
     }
 
+}
     private void subSubsets(int[] nums, int s, List<Integer> list, List<List<Integer>> results) {
-        if (s > nums.length && !list.isEmpty()) 
-            results.add(new ArrayList<Integer>(list));
+        results.add(new ArrayList<Integer>(list));
 
-        for (int i = s; i <= nums.length; ++i) {
-            if (i < nums.length)
-                list.add(nums[i]);
+        for (int i = s; i < nums.length; ++i) {
+            list.add(nums[i]);
             subSubsets(nums, i + 1, list, results);
-            if (i < nums.length)
             list.remove(list.size() - 1);
         }
     }
