f4ede6bee446b98d31025ef696b83a14dcb4637b
kyohiro <kyohiro@gmail.com>
2015-05-29 22:41:01 +0800

    add solution 094

---
+++ 094-binary tree inorder traversal/Solution094.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    //Attempt1: recursive
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) 
+            return new ArrayList<Integer>(); 
+        else {
+            List<Integer> left = inorderTraversal(root.left);
+            left.add(root.val);
+            left.addAll(inorderTraversal(root.right));
+            return left;
+        }
+    }
+
+    //Attempt2: loop with stack
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> results = new ArrayList<Integer>();
+        if (root == null)
+            return results;
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode current = root;
+        while (!stack.empty() || current != null) {
+            if (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            else {
+                current = stack.pop();
+                results.add(current.val);
+                current = current.right;
+            }
+        }
+        return results;
+    }
+}
