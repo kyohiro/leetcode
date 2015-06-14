6757952c1814c360e1633fbeb6584d9b2aba2b03
kyohiro <kyohiro@gmail.com>
2015-06-14 22:41:53 +0800

    update solution 221

--- 221-maximal square/Solution221.java
+++ 221-maximal square/Solution221.java
@@ -24,4 +24,38 @@ public class Solution221 {
         }
         return max * max;
     }
+
+    //Attempt2: DP
+    public int maximalSquare2(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return 0;
+
+        int max = 0;
+        int[][] dp = new int[matrix.length][matrix[0].length];
+
+        for (int i = 0; i < matrix.length; ++i) {
+            if (matrix[i][0] == '1') {
+                max = 1;
+                dp[i][0] = 1;
+            }
+        }
+        for (int i = 0; i < matrix[0].length; ++i) {
+            if (matrix[0][i] == '1') {
+                max = 1;
+                dp[0][i] = 1;
+            }
+        }
+
+        for (int i = 1; i < matrix.length; ++i) {
+            for (int j = 1; j < matrix[i].length; ++j) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1]) + 1;
+                    max = Math.max(dp[i][j], max);
+                }
+            }
+        }
+
+        return max * max;
+    }
 }
